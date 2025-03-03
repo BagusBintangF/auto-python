a10df435fa864d4fdb05e7f581ecc811cb617d00
BagusBintangF <bagusbintang77@gmail.com>
2025-03-03 15:35:57 +0700

    :sparkles: add handling ignoring keyboard interupt and create listener for quit auto :zap:

--- auto/watcher.py
+++ auto/watcher.py
@@ -1,6 +1,9 @@
 import subprocess
 import sys
 import time
+import signal
+import keyboard
+import subprocess
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
@@ -13,7 +16,7 @@ class ReloadHandler(FileSystemEventHandler):
         return subprocess.Popen([sys.executable,"-i", self.script])
     
     def restart_script(self):
-        print(f"[auto]{self.script} changed, restarting...")
+        print(f"[auto] {self.script} changed, restarting...")
         self.process.terminate()
         self.process = self.start_script()
 
@@ -22,18 +25,30 @@ class ReloadHandler(FileSystemEventHandler):
             self.restart_script()
 
 def watch(script):
-    print(f"[auto] watching {script} for changes...")
+    print(f"[auto] Watching {script} for changes...")
+    print(f"[auto] Press ALT + Q to quit the auto....")
     event_handler = ReloadHandler(script)
     observer = Observer()
     observer.schedule(event_handler, path=".", recursive=False)
     observer.start()
 
+    signal.signal(signal.SIGINT, lambda signum, frame:print("[auto] Ignoring Keybaoard Interupt...."))
+
     try:
         while True:
-            time.sleep(1)
-    except KeyboardInterrupt:
+            time.sleep(0.5)
+
+            if keyboard.is_pressed("alt+q"):
+                print(f"[auto] ALT + Q pressed.... ")
+                break
+
+    # except KeyboardInterrupt:
+    #     print(f"Keyboard Interupt")
+    finally:
         observer.stop()
         event_handler.process.terminate()
+        print(f"[auto] Shutdown the observer and close {script} .....")
+        sys.exit(0)
     observer.join() 
     
 
