f6adfb98aa9700d160e942a8a58447cb2a58e568
BagusBintangF <bagusbintang77@gmail.com>
2025-03-03 15:38:13 +0700

    :sparkles: update setup, add install requires keyboard :zap:

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="auto",
     version="0.0.1",
     packages=find_packages(),
-    install_requires=["watchdog"],
+    install_requires=["watchdog", "keyboard"],
     entry_points={
         "console_scripts": [
             "auto = auto.main:main",
